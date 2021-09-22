2ce83363c947e582b1da689faf2f15e6d41a67b4
Giovanni <gionneves@gmail.com>
2021-09-22 10:31:39 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import Home from "./routes/Home.svelte";
+	import Sobre from "./routes/Sobre.svelte";
+	import Blog from "./routes/Blog.svelte";
+
+	export let url = "";
+</script>
+
+<Router {url}>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="Sobre">Sobre</Link>
+		<Link to="blog">Blog</Link>
+	</nav>
+	<div>
+		<Route path="blog/:id" component={BlogPost} />
+		<Route path="blog" component={Blog} />
+		<Route path="Sobre" component={Sobre} />
+		<Route path="/"><Home /></Route>
+	</div>
+</Router>
