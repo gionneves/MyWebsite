cffe8715efcbaf0c2bfe48778abff0549e3c51f5
Giovanni <gionneves@gmail.com>
2021-09-22 17:55:58 -0300

    Save

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { Router, Link, Route } from "svelte-routing";
 	import Home from "./routes/Home.svelte";
-	import Sobre from "./routes/Sobre.svelte";
+	import Portfolio from "./routes/Portfolio.svelte";
 
 	export let url = "";
 </script>
@@ -9,12 +9,12 @@
 <Router {url}>
 	<nav>
 		<Link class="text-padding" to="/">Home</Link>
-		<Link class="text-padding" to="Sobre">Sobre</Link>
+		<Link class="text-padding" to="Portfolio">Portfólio</Link>
 		<Link class="text-padding" to="Videos">Vídeos</Link>
 		<Link class="text-padding" to="Downloads">Downloads</Link>
 	</nav>
 	<div>
-		<Route path="Sobre" component={Sobre} />
+		<Route path="Portfolio" component={Portfolio} />
 		<Route path="/" component={Home} />
 	</div>
 </Router>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,1 +1,24 @@
-<h1>Home</h1>
+<script>
+</script>
+
+<div>
+    <div class="container">
+        <div class="paragraph-title">
+            <h1>Bem-vindo!</h1>
+        </div>
+        <p class="paragraph">
+            Olá, seja bem-vindo, sou o Giovanni e esse é o meu site. Hoje em dia
+            eu não vejo muitos sites pessoais voltados realmente para fins
+            próprios de ser pessoal. A maior parte das vezes vejo mais como um
+            portifólio e não muito algo que a pessoa queira compartilhar (<small
+                >acho que isso se encaixa mais em um blog</small
+            >).
+        </p>
+        <p class="paragraph">
+            Caso você tenha vindo no meu site por questões profissionais, clique
+            em <strong>Portfólio</strong> na barra superior do site. Caso tenha
+            vindo somente pelo blog ou mais sobre mim pode se sentir a vontade
+            de me conhecer um <i>pouquinho</i> mais.
+        </p>
+    </div>
+</div>
