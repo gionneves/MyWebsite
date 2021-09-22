8760ed5e9e6f6db83f41d729d6699933fedfaf57
Giovanni <gionneves@gmail.com>
2021-09-22 11:52:34 -0300

    CSS updates + Home + Sobre Editado: > App.Svelte >> Colocado novas abas como "Vídeo" e "Downloads" >> CSS próprio para Navbar
    >global.css
    >> Comentado código antigo
    >> Adicionado novos códigos para tag "<a>"

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,27 @@
 	import { Router, Link, Route } from "svelte-routing";
 	import Home from "./routes/Home.svelte";
 	import Sobre from "./routes/Sobre.svelte";
-	import Blog from "./routes/Blog.svelte";
 
 	export let url = "";
 </script>
 
 <Router {url}>
 	<nav>
-		<Link to="/">Home</Link>
-		<Link to="Sobre">Sobre</Link>
-		<Link to="blog">Blog</Link>
+		<Link class="text-padding" to="/">Home</Link>
+		<Link class="text-padding" to="Sobre">Sobre</Link>
+		<Link class="text-padding" to="Videos">Vídeos</Link>
+		<Link class="text-padding" to="Downloads">Downloads</Link>
 	</nav>
 	<div>
-		<Route path="blog/:id" component={BlogPost} />
-		<Route path="blog" component={Blog} />
 		<Route path="Sobre" component={Sobre} />
-		<Route path="/"><Home /></Route>
+		<Route path="/" component={Home} />
 	</div>
 </Router>
+
+<style>
+	nav {
+		padding: 0.7em;
+		background-color: #2d2d2d;
+		text-align: center;
+	}
+</style>
